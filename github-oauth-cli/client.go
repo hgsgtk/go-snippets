22328480ac80797bc2b4f8513cb2212bdca73a18
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2018-05-03 23:17:58 +0900

    github-oauth-cli

---
+++ github-oauth-cli/client.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+	"os"
+	"net/http"
+	"io"
+	"context"
+	"github.com/skratchdot/open-golang/open"
+	"encoding/json"
+	"io/ioutil"
+	"fmt"
+	"strings"
+	"github.com/joho/godotenv"
+)
+
+var redirectURL = "http://localhost:18888"
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	state := os.Getenv("STATE")
+
+	conf := &oauth2.Config{
+		ClientID: clientID,
+		ClientSecret: clientSecret,
+		Scopes: []string{"user:email", "gist"},
+		Endpoint: github.Endpoint,
+	}
+	var token *oauth2.Token
+
+	file, err := os.Open("access_token.json")
+	if os.IsNotExist(err) {
+		url := conf.AuthCodeURL(state, oauth2.AccessTypeOnline)
+
+		code := make(chan string)
+		var server *http.Server
+		server = &http.Server{
+			Addr: ":18888",
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "text/html")
+				io.WriteString(w, "<html><script>window.open('about:blank', '_self').close()</script></html>")
+				w.(http.Flusher).Flush()
+				code <- r.URL.Query().Get("code")
+				server.Shutdown(context.Background())
+				}),
+			}
+			go server.ListenAndServe()
+		open.Start(url)
+		token, err = conf.Exchange(oauth2.NoContext, <-code)
+		if err != nil {
+			panic(err)
+		}
+
+		file, err := os.Create("access_token.json")
+		if err != nil {
+			panic(err)
+		}
+		json.NewEncoder(file).Encode(token)
+	} else if err == nil {
+		token = &oauth2.Token{}
+		json.NewDecoder(file).Decode(token)
+	} else {
+		panic(err)
+	}
+	client := oauth2.NewClient(oauth2.NoContext, conf.TokenSource(oauth2.NoContext, token))
+
+	resp, err := client.Get("https://api.github.com/user/emails")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	emails, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(emails))
+
+	gist := `{
+		"description": "API Post example posted by oauth2 client",
+		"public": true,
+		"files" : {
+			"hello_from_rest_api.txt": {
+				"content": "Hello World"
+			}
+		}
+	}`
+	resp2, err := client.Post("https://api.github.com/gists", "application/json", strings.NewReader(gist))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp2.Status)
+	defer resp2.Body.Close()
+	type GistResult struct {
+		Url string `json:"html_url"`
+	}
+	gistResult := &GistResult{}
+	err = json.NewDecoder(resp2.Body).Decode(&gistResult)
+	if err != nil {
+		panic(err)
+	}
+	if gistResult.Url != "" {
+		open.Start(gistResult.Url)
+	}
+}
