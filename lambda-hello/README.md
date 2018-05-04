0561a0136e6524be5d1ae542622f8375de7f58db
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2018-05-04 18:38:41 +0900

    labmda-hello

---
+++ lambda-hello/README.md
@@ -0,0 +1,14 @@
+# lambda-hello
+## how to build
+
+- build & zip
+
+```bash
+$ make build
+```
+
+- upload zip file to lambda
+
+## Ref
+- ['AWS Lambda で Go が使えるようになったので試してみた'](https://dev.classmethod.jp/cloud/aws/aws-lambda-supports-go/)
+- [aws/aws-lambda-go](https://github.com/aws/aws-lambda-go/tree/master/lambda)
---
+++ lambda-hello/hello.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/aws/aws-lambda-go/lambda"
+)
+
+type Event struct {
+    Name string `json:"What is your name?"`
+}
+
+type Response struct {
+    Message string `json:"Answer:"`
+}
+
+func hello(event Event) (Response, error) {
+    return Response{Message: fmt.Sprintf("Hello %s!!", event.Name)}, nil
+}
+
+func main() {
+    lambda.Start(hello)
+}
