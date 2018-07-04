4fdf5b3342d3e82d2611feddbbc1dab405940e14
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2018-07-05 06:29:23 +0900

    elementui-on-go try datePicker, slider, form

--- elementui-on-go/frontend/src/App.vue
+++ elementui-on-go/frontend/src/App.vue
@@ -1,120 +1,50 @@
 <template>
   <div id="app">
-    <el-container style="height: 500px; border: 1px solid #eee">
-  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-    <el-menu :default-openeds="['1', '3']">
-      <el-submenu index="1">
-        <template slot="title"><i class="el-icon-message"></i>Navigator One</template>
-        <el-menu-item-group>
-          <template slot="title">Group 1</template>
-          <el-menu-item index="1-1">Option 1</el-menu-item>
-          <el-menu-item index="1-2">Option 2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="Group 2">
-          <el-menu-item index="1-3">Option 3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">Option4</template>
-          <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-submenu index="2">
-        <template slot="title"><i class="el-icon-menu"></i>Navigator Two</template>
-        <el-menu-item-group>
-          <template slot="title">Group 1</template>
-          <el-menu-item index="2-1">Option 1</el-menu-item>
-          <el-menu-item index="2-2">Option 2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="Group 2">
-          <el-menu-item index="2-3">Option 3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="2-4">
-          <template slot="title">Option 4</template>
-          <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-submenu index="3">
-        <template slot="title"><i class="el-icon-setting"></i>Navigator Three</template>
-        <el-menu-item-group>
-          <template slot="title">Group 1</template>
-          <el-menu-item index="3-1">Option 1</el-menu-item>
-          <el-menu-item index="3-2">Option 2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="Group 2">
-          <el-menu-item index="3-3">Option 3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="3-4">
-          <template slot="title">Option 4</template>
-          <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-    </el-menu>
-  </el-aside>
-  
-  <el-container>
-    <el-header style="text-align: right; font-size: 12px">
-      <el-dropdown>
-        <i class="el-icon-setting" style="margin-right: 15px"></i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>View</el-dropdown-item>
-          <el-dropdown-item>Add</el-dropdown-item>
-          <el-dropdown-item>Delete</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <span>Tom</span>
-    </el-header>
-    
-    <el-main>
-      <el-table :data="tableData">
-        <el-table-column prop="date" label="Date" width="140">
-        </el-table-column>
-        <el-table-column prop="name" label="Name" width="120">
-        </el-table-column>
-        <el-table-column prop="address" label="Address">
-        </el-table-column>
-      </el-table>
-    </el-main>
-  </el-container>
-</el-container>
+    <div class="block">
+      <span class="slider">Slider</span>
+      <el-slider v-model="ruleForm.age"></el-slider>
+    </div>
+    <div>{{ ruleForm.age }}</div>
 
-<style>
-  .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    color: #333;
-  }
-</style>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="120px" class="demo-ruleForm">
+      <el-form-item label="年齢" prop="age">
+        <el-input type="number" v-model="ruleForm.age" auto-complete="off"></el-input>
+      </el-form-item>
+    </el-form>
 
-<script>
-  export default {
-    data() {
-      const item = {
-        date: '2016-05-02',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
-      };
-      return {
-        tableData: Array(20).fill(item)
-      }
-    }
-  };
-</script>
+    <span class="datepicker">日付</span>
+    <el-date-picker
+      v-model="value2"
+      type="date"
+      placeholder="choice date">
+    </el-date-picker>
+  </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It works!',
-        type: 'success',
-        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
-      })
+  name: 'app',
+  data() {
+    return {
+      ruleForm: {
+        age: 0,
+      },
+      rules: {
+        age: [
+          {
+            validator: (rule, value, callback) => {
+              if (!value) {
+                return callback(new Error('値を入力してください'));
+              }
+              if (value > 51) {
+                return callback(new Error('年齢は50歳以下にしてください'))
+              }
+            },
+            trigger: 'change'
+          }
+        ]
+      },
+      value2: '',
     }
   }
 }
--- elementui-on-go/frontend/src/main.js
+++ elementui-on-go/frontend/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+import 'element-ui/lib/locale/lang/ja'
 import App from './App.vue'
 
 Vue.use(ElementUI)
