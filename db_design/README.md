17071fbf0da266a4cc52235f15b06f4894bd032f
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2018-07-22 14:49:00 +0900

    feature/basic implement completly

--- db_design/README.md
+++ db_design/README.md
@@ -4,6 +4,27 @@ sql.DBをどう扱うかについての調査
 ## Setup
 - create database and run seed.sql
 
+## Exec
+- start server
+
+```
+$ go run main.go
+
+$ curl -i localhost:3000/books/show?isbn=978-1503261969
+$ curl -i -X POST -d "isbn=978-1470184841&title=Metamorphosis&author=Franz Kafka&price=5900" localhost:3000/books/create
+
+```
+
+## Todo
+- [x] basic implement: dbを扱うhttpサービスの実装
+
+
+## Tips
+- `sql.Open`はconnectionをcheckしてくれるわけではないので、`db.Ping`で実際にconnectionが通っているかを確認するとよい。
+- `Request.FormValue()` method to fetch the value from the request query string.
+- parameterの値をキーにfetchする場合は、prepared statementを作ることによってescapeしてくれるのでSQL injection対策となる
+- `row.Scan`はレコードがない場合は、`ErrNoRows`というerror型を返すのでエラーハンドリングする
+- https://github.com/jmoiron/sqlx
 
 ## Ref
 - [Practical Persistence in Go: SQL Databases](https://www.alexedwards.net/blog/practical-persistence-sql)
--- db_design/basic/main.go
+++ db_design/basic/main.go
@@ -4,7 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
+	"net/http"
+	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -17,19 +18,34 @@ type Book struct {
 	price  int
 }
 
-func main() {
-	db, err := sql.Open("sqlite3", "./sample.sqlite3")
+var db *sql.DB
+
+func init() {
+	var err error
+	db, err = sql.Open("sqlite3", "./sample.sqlite3")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
-	if err := db.Ping(); err != nil {
+	if err = db.Ping(); err != nil {
 		log.Fatal(err)
 	}
+}
 
+func main() {
+	http.HandleFunc("/books", booksIndex)
+	http.HandleFunc("/books/show", booksShow)
+	http.HandleFunc("/books/create", booksCreate)
+	http.ListenAndServe(":3000", nil)
+}
+
+func booksIndex(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
 	rows, err := db.Query("SELECT * FROM books")
 	if err != nil {
-		fmt.Fprint(os.Stderr, err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	defer rows.Close()
 
@@ -38,15 +54,77 @@ func main() {
 		bk := new(Book)
 		err := rows.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price)
 		if err != nil {
-			fmt.Fprint(os.Stderr, err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		bks = append(bks, bk)
 	}
 	if err := rows.Err(); err != nil {
-		fmt.Fprint(os.Stderr, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	for _, bk := range bks {
-		fmt.Fprintf(os.Stdout, "%s, %s, %s, %d円\n", bk.isbn, bk.title, bk.author, bk.price)
+		fmt.Fprintf(w, "%s, %s, %s, %d円\n", bk.isbn, bk.title, bk.author, bk.price)
 	}
+}
+
+func booksShow(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	isbn := r.FormValue("isbn")
+	if isbn == "" {
+		http.Error(w, "paramter: isbn is necessary.", http.StatusBadRequest)
+		return
+	}
+
+	row := db.QueryRow("SELECT * FROM books WHERE isbn = ?", isbn)
+
+	bk := new(Book)
+	err := row.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price)
+	if err == sql.ErrNoRows {
+		http.NotFound(w, r)
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "%s, %s, %s, %d円\n", bk.isbn, bk.title, bk.author, bk.price)
+}
+
+func booksCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	isbn := r.FormValue("isbn")
+	title := r.FormValue("title")
+	author := r.FormValue("author")
+	if isbn == "" || title == "" || author == "" {
+		http.Error(w, "paramter: isbn, title, author is necessary.", http.StatusBadRequest)
+		return
+	}
+	price, err := strconv.ParseInt(r.FormValue("price"), 0, 0)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result, err := db.Exec("INSERT INTO books VALUES(?, ?, ?, ?)", isbn, title, author, price)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	rowAffected, err := result.RowsAffected()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Book %s created successfully (%d row affected)\n", isbn, rowAffected)
 }
