7dc950daad2e52a28914051e54cb07844e31ea3f
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2019-02-03 03:26:29 +0900

    impl if linter by base blog.

---
+++ iflinter/README.md
@@ -0,0 +1,3 @@
+# if linter
+
+refs: https://devblog.thebase.in/entry/2018/12/24/110000
---
+++ iflinter/cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+)
+
+func main() {
+	fst := token.NewFileSet()
+	f, err := parser.ParseFile(fst, "sample.go", nil, 0)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	ast.Inspect(f, func(n ast.Node) bool {
+
+		expr, ok := n.(*ast.BinaryExpr)
+		if !ok {
+			return true
+		}
+
+		if expr.Op == token.EQL {
+			xIdent, ok := expr.X.(*ast.Ident)
+			if !ok {
+				return true
+			}
+			yIdent, ok := expr.Y.(*ast.Ident)
+			if !ok {
+				return true
+			}
+
+			xString := xIdent.String()
+			yString := yIdent.String()
+
+			switch yString {
+			case "true":
+				fmt.Printf("- got: %s == %s\n+want: %s\n", xString, yString, xString)
+			case "false":
+				fmt.Printf("- got: %s == %s\n+ want: !%s\n", xString, yString, xString)
+			}
+
+			return true
+		}
+		return true
+	})
+}
