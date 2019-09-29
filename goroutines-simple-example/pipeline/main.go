06bf4ead85bff76891584fce9ab8361d4ce71123
hgsgtk <hgsgtk@gmail.com>
2019-09-29 11:58:53 +0900

    use close

--- goroutines-simple-example/pipeline/main.go
+++ goroutines-simple-example/pipeline/main.go
@@ -6,19 +6,26 @@ func main() {
 	naturals := make(chan int)
 	squares := make(chan int)
 
+	// Counter
 	go func() {
-		for x := 0; ; x++ {
+		for x := 0; x < 100; x++ {
 			naturals <- x
 		}
+		close(naturals)
 	}()
 
+	// Square
 	go func() {
 		for {
-			x := <-naturals
+			x, ok := <-naturals
+			if !ok {
+				break
+			}
 			squares <- x * x
 		}
 	}()
 
+	// Printer
 	for {
 		fmt.Println(<-squares)
 	}
