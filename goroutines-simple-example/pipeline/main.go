d5272d52ec70411c45bdf34d4d2a8a3751a62332
hgsgtk <hgsgtk@gmail.com>
2019-09-29 12:07:57 +0900

    divide into functions

--- goroutines-simple-example/pipeline/main.go
+++ goroutines-simple-example/pipeline/main.go
@@ -6,27 +6,27 @@ func main() {
 	naturals := make(chan int)
 	squares := make(chan int)
 
-	// Counter
-	go func() {
-		for x := 0; x < 100; x++ {
-			naturals <- x
-		}
-		close(naturals)
-	}()
+	go counter(naturals)
+	go squarer(squares, naturals)
+	printer(squares)
+}
 
-	// Square
-	go func() {
-		for {
-			x, ok := <-naturals
-			if !ok {
-				break
-			}
-			squares <- x * x
-		}
-	}()
+func counter(out chan<- int) {
+	for x := 0; x < 100; x++ {
+		out <- x
+	}
+	close(out)
+}
 
-	// Printer
-	for {
-		fmt.Println(<-squares)
+func squarer(out chan<- int, in <-chan int) {
+	for v := range in {
+		out <- v * v
+	}
+	close(out)
+}
+
+func printer(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
 	}
 }
