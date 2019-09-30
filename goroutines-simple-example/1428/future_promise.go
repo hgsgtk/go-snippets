ed10a9749e50ce1e58ecf315b7051e25fcd6ec01
hgsgtk <hgsgtk@gmail.com>
2019-09-30 21:46:53 +0900

    1428 cache

--- goroutines-simple-example/1428/future_promise.go
+++ goroutines-simple-example/1428/future_promise.go
@@ -8,31 +8,65 @@ import (
 
 // 依存関係のあるタスクを表現する
 
-func readFile(path string) chan string {
-	promise := make(chan string)
+type StringFuture struct {
+	receiver chan string
+	cache    string
+}
+
+func NewStringFuture() (*StringFuture, chan string) {
+	f := &StringFuture{
+		receiver: make(chan string),
+	}
+	return f, f.receiver
+}
+
+func (f *StringFuture) Get() string {
+	r, ok := <-f.receiver
+	if ok {
+		close(f.receiver)
+		f.cache = r
+		return f.cache
+	}
+	return f.cache
+}
+
+func (f *StringFuture) Close() {
+	close(f.receiver)
+}
+
+func readFile(path string) *StringFuture {
+	promise, future := NewStringFuture()
 	go func() {
 		content, err := ioutil.ReadFile(path)
 		if err != nil {
 			fmt.Printf("read error %s\n", err.Error())
-			close(promise)
+			promise.Close()
 		} else {
-			promise <- string(content)
+			future <- string(content)
 		}
 	}()
 	return promise
 }
 
-func printFunc(futureSource chan string) chan []string {
+func printFunc(futureSource *StringFuture) chan []string {
 	promise := make(chan []string)
 	go func() {
 		var result []string
 
-		for _, line := range strings.Split(<-futureSource, "\n") {
+		for _, line := range strings.Split(futureSource.Get(), "\n") {
 			if strings.HasPrefix(line, "func ") {
 				result = append(result, line)
 			}
 		}
 		promise <- result
+	}()
+	return promise
+}
+
+func countLines(futureSource *StringFuture) chan int {
+	promise := make(chan int)
+	go func() {
+		promise <- len(strings.Split(futureSource.Get(), "\n"))
 	}()
 	return promise
 }
@@ -43,4 +77,5 @@ func main() {
 	// ソースを受け取り関数宣言のリストを返す
 	futureFuncs := printFunc(futureSource)
 	fmt.Println(strings.Join(<-futureFuncs, "\n"))
+	fmt.Println(<-countLines(futureSource))
 }
