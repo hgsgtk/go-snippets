8219e0dfd9f9be1610ca50d2157e8542a3115ee3
hgsgtk <kazuki@autify.com>
2022-03-14 17:17:28 +0900

    os.Exit

--- random-port/main.go
+++ random-port/main.go
@@ -4,19 +4,18 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"os"
 )
 
 func main() {
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Printf("error on listening a random port: %s\n", err)
-		os.Exit(1)
+		return
 	}
 
 	log.Printf("using port: %d", l.Addr().(*net.TCPAddr).Port)
 	if err := http.Serve(l, nil); err != nil {
 		log.Printf("error on serving: %s\n", err)
-		os.Exit(1)
+		return
 	}
 }
