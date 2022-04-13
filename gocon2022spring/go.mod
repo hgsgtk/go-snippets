09aa2b14f4bf38838953bb783c15b371e0070ba0
hgsgtk <kazuki@autify.com>
2022-04-13 13:50:12 +0900

    feat: hijack HTTP

---
+++ gocon2022spring/go.mod
@@ -0,0 +1,3 @@
+module github.com/hgsgtk/go-snippets/gocon2022spring
+
+go 1.18
---
+++ gocon2022spring/hijack/hijack.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+)
+
+type hijackHandler struct{}
+
+func (h *hijackHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	hj, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "webserver doesn't support hijacking", http.StatusInternalServerError)
+		return
+	}
+	conn, _, err := hj.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer conn.Close()
+
+	resp := []byte("HTTP/1.0 200 OK\r\n\r\nGopher at your service\r\n")
+	if _, err := conn.Write(resp); err != nil {
+		return
+	}
+}
+
+func main() {
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+
+	fmt.Fprintf(os.Stdout, "server on %q", l.Addr().String())
+	if err := http.Serve(l, &hijackHandler{}); err != nil {
+		panic(err)
+	}
+}
