bed20b61e6eba3295641312181a6ff7491c39201
hgsgtk <kazuki@autify.com>
2022-09-02 17:28:38 +0900

    html: 80

--- html/main.go
+++ html/main.go
@@ -84,5 +84,5 @@ func main() {
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
