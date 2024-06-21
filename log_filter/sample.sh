14b4fc89f591210f1f585770bcefafe503ec2623
hgsgtk <kazuki@autify.com>
2024-06-22 03:26:30 +0900

    sample.sh

--- log_filter/sample.sh
+++ log_filter/sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-log_file_path="./chromedriver.log"
-another_path="./another.log"
+log_file_path="./tmp/chromedriver.log"
+another_path="./tmp/another.log"
 touch $log_file_path
 chromedriver --log-path=$log_file_path &
 tail -f $log_file_path | tee -a $another_path
